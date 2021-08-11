b08ba698c8c86f8d009d52222819488dc4b5c138
Eric Fithian <ericfithian@Erics-MacBook-Pro.local>
2021-08-11 16:48:30 -0400

    Duplicated work with the vaccines

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('testing/', views.TestListView.as_view(), name='testing'),
     path('testing/<int:id>/', views.testing_lookup, name='testing-detail'),
     path('testing/<int:id>/delete/', views.testing_delete, name='testing-delete'),
-    path('vaccines/', views.vaccines, name='vaccines'),
+    path('vaccines/', views.VaccineListView.as_view(), name='vaccines'),
+    path('vaccines/<int:id>/', views.vaccines_lookup, name='vaccine-detail'),
+    path('vaccines/<int:id>/delete/', views.vaccines_delete, name='vaccine-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Test
+from .models import Test, Vaccine
 from django.views import View
 
 #Here are my class based views
@@ -11,6 +11,14 @@ class TestListView(View):
             'objects': queryset
         }
         return render(request, 'tests/test_list.html', context)
+
+class VaccineListView(View):
+    def get(self, request):
+        queryset = Vaccine.objects.all()
+        context = {
+            'objects': queryset
+        }
+        return render(request, 'vaccines/vaccine_list.html', context)
 
 # Create your function views here.
 
@@ -38,5 +46,20 @@ def testing_delete(request, id):
     }
     return render(request, 'tests/test_delete.html', context)
 
-def vaccines(request):
-    return render(request, 'vaccines.html')
+def vaccines_lookup(request, id):
+    obj = get_object_or_404(Vaccine, id=id)
+    context = {
+        'object': obj
+    }
+    return render(request, 'vaccines/vaccine_detail.html', context)
+
+def vaccines_delete(request, id):
+    obj = get_object_or_404(Vaccine, id=id)
+    if request.method == "POST":
+        #Building this in to make sure they don't delete just by going
+        obj.delete()
+        return redirect('../../')
+    context = {
+        "object": obj
+    }
+    return render(request, 'vaccines/vaccine_delete.html', context)
